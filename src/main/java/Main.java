aaece66f08d1b0f1432264bdaa5386c235b7aeac
volkovlr.24 <volkovlr@mail.ru>
2022-12-01 12:40:38 +0300

    events consumer works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,8 @@ import io.github.humbleui.jwm.App;
 public class Main {
 
     public static void main(String[] args) {
-        App.start(Application::new);
+
+        App.start(Application ::new);
     }
 
 }
