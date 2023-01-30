0470c740fe36d8d0f15d5390635cfeb598cd0b16
volkovlr.24 <volkovlr@mail.ru>
2023-01-30 12:13:55 +0300

    text centering works

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -44,9 +44,11 @@ public class Application implements Consumer<Event> {
     public Application() {
         // создаём окно
         window = App.makeWindow();
+        // создаём первый заголовок
+        label = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
+                "Привет, мир!", true, true);
         // задаём обработчиком событий текущий объект
         window.setEventListener(this);
-        label = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, "Привет, мир!");
 
         // делаем окно видимым
         // задаём размер окна
